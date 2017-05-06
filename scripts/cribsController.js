836d6d4871babe0b3f4acfa4a1688f66ef29e8e3
sensei100 <jenanton01@gmail.com>
2017-05-06 10:32:55 -0400

    create cribsFactory

--- scripts/cribsController.js
+++ scripts/cribsController.js
@@ -1,34 +1,21 @@
-(function() { 
-  'use strict';
-
-  function CribsController($scope) {
-
-        $scope.cribs = [
-          {
-            "type": "Condo",
-            "price": 220000,
-            "address": "213 Grove Street",
-            "description": "Excellent place, really nice view!"
-          },
-          {
-            "type": "House",
-            "price": 410500,
-            "address": "7823 Winding Way",
-            "description": "Beautiful home with lots of space for a large family."
-          },
-          {
-            "type": "Duplex",
-            "price": 395000,
-            "address": "834 River Lane",
-            "description": "Great neighborhood and lots of nice green space."
-          }
-        ];
-  }
-
-    CribsController.$inject = ['$scope']
-
-  angular
+angular
     .module('ngCribs')
-    .controller('cribsController', CribsController);
+    .controller('cribsController', function($scope, cribsFactory) {
 
-  }());
+        $scope.cribs;
+
+        cribsFactory.getCribs().success(function(data) {
+          $scope.cribs = data;
+        }).error(function(error) {
+          console.log(error);
+        });
+
+        $scope.sayHello = function() {
+          console.log("Hello");
+        }
+  });
+
+    
+
+  
+ 
---
+++ scripts/cribsFactory.js
@@ -0,0 +1,16 @@
+angular
+    .module('ngCribs')
+    .factory('cribsFactory', function($http) {
+
+      function getCribs() {
+        return $http.get('data/data.json');
+      }
+
+        return {
+        getCribs: getCribs
+      }
+
+  });
+
+  
+
